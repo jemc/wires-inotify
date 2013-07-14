7e452db294a9b5b7b39a3b30f37e7e03dc10fdec
Joe McIlvain <joe.eli.mac@gmail.com>
2013-07-14 13:59:25 -0400

    Basic skeleton NotifyHub class object

--- lib/wires-inotify.rb
+++ lib/wires-inotify.rb
@@ -4,7 +4,41 @@ module Wires
   
   class NotifyEvent < Event; end
   
-  class Notifier
+  class NotifyHub
+    
+    class << self
+      
+      def state;  @state         end
+      def alive?; @state==:alive end
+      def dead?;  @state==:dead  end
+      
+      def class_init()
+        @notifier = INotify::Notifier.new
+        
+        @state = :dead
+        
+        Hub.after_run(retain:true) do
+          @state = :alive
+          @thread = Thread.new { while alive?; thread_iter; end }
+        end
+        
+        Hub.before_kill(retain:true) do
+          @state = :dead
+          sleep 0 while @thread.status
+          @thread = nil
+        end
+        
+      end
+      
+    private
+      
+      def thread_iter
+        puts "alive!!!"
+      end
+      
+    end
+    
+    class_init
   end
   
 end
--- spec/notifier_spec.rb
+++ spec/notifier_spec.rb
@@ -7,5 +7,45 @@ require 'minitest/spec'
 include Wires
 
 
-describe Notifier do
+describe NotifyHub do
+  
+  it "is alive when and only when the Hub is." do
+    
+    NotifyHub.alive?.must_equal Hub.alive?
+    NotifyHub.dead?.must_equal  Hub.dead?
+    NotifyHub.state.must_equal :dead
+    NotifyHub.instance_variable_get(:@thread).must_be_nil
+    
+    Hub.run
+    
+    NotifyHub.alive?.must_equal Hub.alive?
+    NotifyHub.dead?.must_equal  Hub.dead?
+    NotifyHub.state.must_equal :alive
+    NotifyHub.instance_variable_get(:@thread).wont_be_nil
+    NotifyHub.instance_variable_get(:@thread).status.wont_equal false
+    
+    Hub.kill
+    
+    NotifyHub.alive?.must_equal Hub.alive?
+    NotifyHub.dead?.must_equal  Hub.dead?
+    NotifyHub.state.must_equal :dead
+    NotifyHub.instance_variable_get(:@thread).must_be_nil
+    
+    Hub.run
+    
+    NotifyHub.alive?.must_equal Hub.alive?
+    NotifyHub.dead?.must_equal  Hub.dead?
+    NotifyHub.state.must_equal :alive
+    NotifyHub.instance_variable_get(:@thread).wont_be_nil
+    NotifyHub.instance_variable_get(:@thread).status.wont_equal false
+    
+    Hub.kill
+    
+    NotifyHub.alive?.must_equal Hub.alive?
+    NotifyHub.dead?.must_equal  Hub.dead?
+    NotifyHub.state.must_equal :dead
+    NotifyHub.instance_variable_get(:@thread).must_be_nil
+    
+  end
+  
 end
