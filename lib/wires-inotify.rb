623149577e7bef8fbca42a18b3d4b749ff1aa02e
Joe McIlvain <joe.eli.mac@gmail.com>
2013-07-14 19:28:56 -0400

    Basic functionality in place

--- lib/wires-inotify.rb
+++ lib/wires-inotify.rb
@@ -1,10 +1,40 @@
 require 'rb-inotify'
 
+def inotify_watch(*args)
+  Wires::NotifyHub.watch(*args)
+end
+
 module Wires
   
-  class NotifyEvent < Event; end
+  class NotifyEvent             < Event; end
+  
+  class NotifyAccessEvent       < NotifyEvent; end
+  class NotifyAttribEvent       < NotifyEvent; end
+  class NotifyModifyEvent       < NotifyEvent; end
+  class NotifyOpenEvent         < NotifyEvent; end
+  class NotifyCreateEvent       < NotifyEvent; end
+  class NotifyDeleteEvent       < NotifyEvent; end
+  class NotifyDeleteSelfEvent   < NotifyEvent; end
+  class NotifyMoveSelfEvent     < NotifyEvent; end
+  
+  class NotifyCloseEvent        < NotifyEvent; end
+  class NotifyCloseWriteEvent   < NotifyCloseEvent; end
+  class NotifyCloseNowriteEvent < NotifyCloseEvent; end
+  
+  class NotifyMoveEvent         < NotifyEvent; end
+  class NotifyMovedFromEvent    < NotifyMoveEvent; end
+  class NotifyMovedToEvent      < NotifyMoveEvent; end
   
   class NotifyHub
+    
+    @@events = EventRegistry.list.select{ |x| (x<NotifyEvent) }
+    
+    @@vestigial_events = @@events.select{ |x| 
+      (ObjectSpace.each_object(Class).select { |c| c < x }.empty?) }
+    
+    @@vestigial_events_as_flags = \
+      @@vestigial_events.map{|x| x.codestring}
+                        .map{|x| x.gsub(/^notify_/, "").to_sym}
     
     class << self
       
@@ -13,27 +43,50 @@ module Wires
       def dead?;  @state==:dead  end
       
       def class_init()
-        @notifier = INotify::Notifier.new
         
         @state = :dead
         
+        @notifier = INotify::Notifier.new
+        
         Hub.after_run(retain:true) do
           @state = :alive
-          @thread = Thread.new { while alive?; thread_iter; end }
+          @thread = Thread.new { while alive?; thread_iter; Thread.pass; end }
         end
         
         Hub.before_kill(retain:true) do
           @state = :dead
-          sleep 0 while @thread.status
+          @thread.kill
           @thread = nil
         end
         
       end
       
+      def watch(path, *flags)
+        flags = [:all_events] if flags.empty?
+        @notifier.watch("/tmp/foo", *flags)
+      end
+      
     private
       
       def thread_iter
-        puts "alive!!!"
+        @notifier.read_events.each { |e| process_event(e) }
+      end
+      
+      def process_event(e)
+        cls = nil
+        if (common = (e.flags&@@vestigial_events_as_flags)).empty?
+          raise NotImplementedError, \
+            "No Wires::NotifyEvent for flags #{e.flags}"
+        else
+          cls = Event.from_codestring("notify_"+common.first.to_s)
+          
+          Channel.new(e.watcher.path)
+                 .fire(cls.new(*e.flags,
+                              name:          e.name,
+                              absolute_name: e.absolute_name,
+                              watchpath:     e.watcher.path))
+          
+        end
       end
       
     end
